6447130c83d38825bbc230b4f175c1ecc1008193
xuepinggui <3220145682@qq.com>
2025-05-24 21:48:16 +0800

    update LightFinder

---
+++ src/armor_detector/include/ArmorFinder.h
@@ -0,0 +1,50 @@
+#ifndef AUTOAIM_ARMORFINDER_H
+#define AUTOAIM_ARMORFINDER_H
+
+#include "opencv2/opencv.hpp"
+#include <vector>
+#include "Log.h"
+
+using namespace cv;
+using namespace std;
+
+namespace nw{
+    static bool status =false;
+    class ArmorBlob{
+        public:
+            ArmorBlob(){
+                corners=vector<point2f>(4);
+            }
+            double confidence;
+            Rect rect;
+            vector<point2f>corners;
+            int _class;
+            double angle;
+            double x,y,z;
+            bool is_big_armor;  
+            //用于set,按(x,y,z)到（0,0,0）的距离排序
+            bool operator <const ArmorBlob& a>const{
+                return x*x+y*y+z*z < a.x*a.x+a.y*a.y+a.z*a.z;
+            }
+        private:
+    }
+    typedef vector<ArmorBlob> ArmorBlobs;
+    class ArmorFinder{
+        public:
+            bool judgeArmor(const ArmorBlob&);
+            bool matchTwoLightBar(const RotatedRect&,const RotatedRect&);
+            bool getArmor(const RotatedRect&,const RotatedRect&,ArmorBlob& armor);
+            Rect getScaleArmorToRoi(const Rect&);
+            vector<int> getExtreme(const ArmorBlob&);
+        private:
+            inline float getAngle(const RotatedRect&);
+            inline bool checkAngleDiff(const RotatedRect& l,const RotatedRect& r);
+            inline bool checkHeightDiff(const RotatedRect& l,const RotatedRect& r);
+            inline bool checkheightMatch(const RotatedRect& l,const RotatedRect& r);
+            inline bool checkHorizontalDistance(const RotatedRect& l,const RotatedRect& r)；
+            inline bool checkDislocation(const RotatedRect& l,const RotatedRect& r);
+            // inline bool checkRatio(const RotatedRect& l, const RotatedRect& r);           
+    };
+
+}
+#endif //AUTOAIM_ARMORFINDER_H
---
+++ src/armor_detector/src/ArmorFinder.cpp
@@ -0,0 +1,107 @@
+#include "ArmorFinder.h"
+using namespace nw;
+
+bool ArmorFinfer::matchTwoLightBar(const RotatedRect& l, const RotatedRect& r) {
+    status=checkAngleDiff(l,r);
+    // DLOG_IF(INFO, !status) << "angle differs: left: " << getAngle(l) << " right: " << getAngle(r);
+    if(!status) return false;
+    status=checkHeightDiff(l,r);
+    // DLOG_IF(INFO, !status) << "height differs: " << abs((l.center-r.center).y);
+    if(!status) return false;
+    // status = checkRatio(l, r);
+    // if(!status) return false;
+    status=checkHeightMatch(l,r);
+    // DLOG_IF(INFO, status) << "height matches: " << l.size.height << " " << r.size.height;
+    if(!status) return false;
+    return true;
+}
+
+bool ArmorFinder::judgeArmor(const ArmorBlob &armor_blob){
+    return armor_blob.rect.size().aspectRatio()<=5&&armor_blob.rect.size().aspectRatio()>1.5;
+}
+
+/**
+ * @brief 将两个灯条拼接成一个装甲
+ * 
+ * @param l 左灯条
+ * @param r 右灯条
+ * @param armor 输出的装甲对象
+ * @return 是否成功拼接装甲（true false）
+ */
+bool ArmorFinder::getArmor(const RotateRect &l, const RotatedRect &r, ArmorBlob &armor) {
+    Point2f points_of_rrect[4];
+    l.points(points_of_rrect):
+    float height=fmax(l.size.width,l.size.height);
+    armor.rect=Rect(l.center.x,l.center.y-height/2, rcenter.x-l.center.x,height);
+    // armor
+    // 0 1
+    // 3 2
+    if (l.angle > 45)
+    {
+        armor.corners[0] = (points_of_rrect[0] + points_of_rrect[1]) / 2;
+        armor.corners[3] = (points_of_rrect[3] + points_of_rrect[2]) / 2;
+    }
+    else
+    {
+        armor.corners[0] = (points_of_rrect[1] + points_of_rrect[2]) / 2;
+        armor.corners[3] = (points_of_rrect[0] + points_of_rrect[3]) / 2;
+    }
+    r.points(points_of_rrect);
+    if (r.angle > 45)
+    {
+        armor.corners[1] = (points_of_rrect[1] + points_of_rrect[0]) / 2;
+        armor.corners[2] = (points_of_rrect[2] + points_of_rrect[3]) / 2;
+    }
+    else
+    {
+        armor.corners[1] = (points_of_rrect[2] + points_of_rrect[1]) / 2;
+        armor.corners[2] = (points_of_rrect[3] + points_of_rrect[0]) / 2;
+    }
+    if(max(armor.corner[0].x,armor.coners[3].x)>min(armor.corners[1].x,armor.corners[2].x)) return flase;
+    return true;
+}
+
+/**
+ * @brief 检查灯条角度是否合理
+ * 
+ * @param l 左灯条
+ * @param r 右灯条
+ * @param angle 角度差
+ * @return 返回角度差
+ */
+bool ArmorFinder::checkAngleDiff(const RotatedRect &l, const RotatedRect &r) {
+    const float & angle_l =getAngle(l);
+    const float & angle_r = getAngle(r);
+    return abs(angle_l - angle_r) < 30.0f;
+}
+
+float armorFinder::getAngle(const RotatedRect &rrect){
+    return rrect.size.width>rrect.size.height ? rrect.angle-90 : rrect.angle;
+}
+
+bool ArmorFinder::checkHeightDiff(const RotatedRect &l, const RotatedRect &r) {
+    const Point2f& diff =l.center - r.center;
+    return abs(diff,y)<min(max(l.size.height,l.size.width),max(r.size.height,r.size.weidth))*1.5;
+}
+
+bool ArmorFinder::checkHorizontalDistace(const RotatedRect &l, const RotatedRect &r) {
+    return false;
+}
+
+bool ArmorFinder::checkDislocation(const RotatedRect &l, const RotatedRect &r) {
+  return false;
+}
+
+vector<int>ArmorFinder::getExtreme(const ArmorBlob& armor) {
+    int x1=min(static_cast<float>(CameraParams::width),max(0.f,min(armor.corners[0].x,armor.corners[3].x)));
+    int y1 = min(static_cast<float>(CameraParams::height), max(0.f, min(armor.corners[0].y, armor.corners[1].y) - min(armor.rect.height, armor.rect.width)/2.5f));
+    int x2=min(static_cast<float>(CameraParams::width),max(0.f,min(armor.corners[2].x,armor.corners[1].x)));
+    int y2 = min(static_cast<float>(CameraParams::height), max(0.f, max(armor.corners[2].y, armor.corners[3].y) + min(armor.rect.height, armor.rect.width)/2.5f));
+    return { x1, y1, x2, y2 };
+}   
+
+bool ArmorFinder::checkHeightMatch(const RotatedRect &l, const RotatedRect &r) {
+    float lh=max(l.size.height,l.size.width);
+    float rh=max(l.size.height,r.size.width);
+    return min(lh,rh)*2>max(lh,rh);
+}
